e93096bc0f83589ef68cfa879733fc930273b320
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-06-16 12:30:52 +0300

    Time: 104 ms (90.24%), Space: 45.5 MB (85.29%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int len = nums.size();
-        return nums.at(len - k);
-
+        sort(nums.begin() , nums.end());
+        return(nums[nums.size()-k]);
     }
 };
+
