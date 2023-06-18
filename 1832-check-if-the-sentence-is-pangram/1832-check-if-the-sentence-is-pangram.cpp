b0d95a7aafec727db561e64aa1a039c1a454104d
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-06-18 17:18:21 +0300

    Time: 4 ms (43.35%), Space: 6.6 MB (53.37%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        sort(sentence.begin() , sentence.end());
+            int x = 97;
+           int ct;
+          for(int i = 0 ; i< sentence.size() ; i+=ct){
+               ct = count(sentence.begin() , sentence.end() , sentence[i]);
+                if( x == sentence[i])
+                     x++;
+                else
+                    return false;
+            }
+             if( x == 123 )
+               return true;
+             else
+               return false;
+        }
+    
+};
