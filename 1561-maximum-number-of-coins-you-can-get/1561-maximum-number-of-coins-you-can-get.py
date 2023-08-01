d3939c26a04424aa9963a7bc0e3a5cb7a3953ad5
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-08-01 10:19:34 +0300

    Time: 551 ms (95.69%), Space: 28.7 MB (77.80%) - LeetHub

---
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort()
+        
+        maxpile=0
+        # amount much pile each person get
+        
+        pil = len(piles)/3
+        #start from index pil then update by two since the maximum is taken by alice and us
+         
+        for i in range (int(pil) , len(piles) , 2):
+            maxpile+=piles[i]
+            
+        return maxpile
