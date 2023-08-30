bb0a2a624bf8a2dee232774e41392eb1bd8a5ded
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-08-30 07:12:22 +0300

    Time: 637 ms (52.45%), Space: 30.4 MB (97.79%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -17,4 +17,31 @@ class Solution:
         return maxval
     
     
-#using kaden's Algorithm can be implmented in O(n) time complexity
+#using sliding window also we can get the index too
+
+#         curr = 0 
+#         maxsum = nums[0]
+        
+#         maxl , maxr  ,  L = 0 , 0  , 0 
+        
+#         for r in range(len(nums)):
+            
+#             if cur < 0:
+#                 cur= 0
+#                 l = r
+                
+#             cur += num[r]
+            
+#             if cur > maxsum:
+#                 masxum=cur
+                
+#                 maxl , maxr = l , r
+                
+#             return [maxl , maxr]
+                
+                
+            
+        
+
+        
+      
