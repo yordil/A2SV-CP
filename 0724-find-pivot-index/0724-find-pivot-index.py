fad067554c152070a82ba470f48c2c5359078b12
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-09-03 16:18:53 +0300

    Time: 130 ms (94.26%), Space: 17.6 MB (35.69%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,52 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        
+        Sum = sum(nums)
+        
+        prefix = 0
+        
+        rightsum = 0 
+        for i in range(len(nums)):
+            
+            prefix +=nums[i]
+            rightsum = Sum - prefix + nums[i]
+            
+            if(prefix == rightsum):
+                return i
+            
+         
+            
+        return -1
+    
+    
+# my previous cpp implmentation   
+
+
+# class Solution {
+# public:
+#     int pivotIndex(vector<int>& nums) {
+        
+#         int left_sum =0 ;
+#         int right = accumulate(nums.begin() , nums.end(), 0);
+#         for(int i= 0 ; i< nums.size() ;++i){
+            
+#             int rightcpy;
+#             left_sum += nums[i];
+#             rightcpy = right - left_sum + nums[i];
+           
+#             if(left_sum == rightcpy)
+#                return i;
+#         }
+               
+#                return -1;
+#     }
+
+# };
+
+            
+        
+        
+      
+            
+            
+        
