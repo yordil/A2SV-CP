315aa762101080c1a825fc4b3b0244178fd72e6a
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-09-05 17:42:21 +0300

    Time: 60 ms (41.93%), Space: 20.8 MB (17.77%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # // my first implmentation using hashset and O(n) memory space
+        
+        # val = 0 ;
+        
+        myhash = set()
+        
+        slow , fast =  head , head
+        
+        while fast and fast.next:
+            
+            if slow in myhash:
+                return slow
+            
+            myhash.add(slow)
+            
+            slow = slow.next
+            fast = fast.next.next
+            
+            
+            
+        return None
