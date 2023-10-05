e259336cba6bd326d03d4f9915193c2228db4b4c
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-05 09:38:59 +0300

    Time: 81 ms (75.00%), Space: 17.7 MB (86.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,7 +1,56 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if target in nums and nums.count(target) > 1:
-            return [nums.index(target) , nums.index(target)+nums.count(target)-1]
-        elif target in nums and nums.count(target) == 1:
-            return [nums.index(target) , nums.index(target)]
-        return [-1 , -1]
+        leftindex = self.binaryhelper(nums , target , False)
+        rightindex = self.binaryhelper(nums , target ,True)
+        return [leftindex , rightindex]
+        
+        
+    def binaryhelper(self, nums , target , forright):
+        left , right  = 0  , len(nums) -1 
+        i = -1;
+        while left <= right:
+
+            mid=  left + (right - left) //2
+
+            if target > nums[mid]:
+                left = mid+1
+
+            elif target < nums[mid]:
+                right = mid -1
+                 
+
+            else:
+                i = mid
+                if forright:
+                    left = mid +1
+                else:
+                    right = mid - 1
+        return i
+        
+        
+        
+        
+        
+        
+        
+        
+                        
+
+            
+        
+    
+    
+    
+    
+    
+    
+    
+    
+#         MY previous implmentation 
+# class Solution:
+#     def searchRange(self, nums: List[int], target: int) -> List[int]:
+#         if target in nums and nums.count(target) > 1:
+#             return [nums.index(target) , nums.index(target)+nums.count(target)-1]
+#         elif target in nums and nums.count(target) == 1:
+#             return [nums.index(target) , nums.index(target)]
+#         return [-1 , -1]
