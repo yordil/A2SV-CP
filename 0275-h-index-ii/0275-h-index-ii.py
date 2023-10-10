67c11ed377f13b2b8e91d84612a64a7db60b98f4
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-10 07:56:31 +0300

    Time: 128 ms (71.37%), Space: 23.1 MB (70.40%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        l, r = 0, len(citations) - 1
+
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            
+            if citations[mid] == len(citations) - mid:
+                return citations[mid]
+            elif citations[mid] > len(citations) - mid:
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return len(citations) - l
