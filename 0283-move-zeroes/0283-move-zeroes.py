92579ca51ad0a9db4967a5ad374a98743dbdecb8
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-12 12:31:29 +0300

    Time: 145 ms (61.72%), Space: 17.8 MB (93.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,16 +4,39 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         left = 0
-        right = len(nums)-1
+        
+        for r in range(len(nums)):
+            
+            if nums[r]:
+                nums[left] , nums[r] = nums[r] , nums[left]
+                
+                left+=1
+                
+        return nums
+            
+            
+            
+                
+              
+            
+            
+            
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         left = 0
+#         right = len(nums)-1
         
         
         
-        for r in range(0 , len(nums)-1):
-            if nums[left] == 0:
-                nums.pop(left)
-                nums.append(0);  
-            else:
-                left = left + 1
+#         for r in range(0 , len(nums)-1):
+#             if nums[left] == 0:
+#                 nums.pop(left)
+#                 nums.append(0);  
+#             else:
+#                 left = left + 1
                 
             
             
@@ -21,6 +44,10 @@ class Solution:
                 
               
             
+                
+                
+                
+        
                 
                 
                 
