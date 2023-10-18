a6d0f9e0d429886bbb522c34fe3165218c669d02
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-18 12:28:56 +0300

    Time: 29 ms (97.05%), Space: 16.2 MB (84.08%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,28 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        copys = []
+        copyt = []
+        
+        for i in range(len(s)):
+            
+            if s[i] == "#":
+                if not copys:
+                    continue
+                else:
+                    copys.pop()
+                
+            else: copys.append(s[i])
+        
+        for i in range(len(t)):
+            
+            if t[i] == "#":
+                if  not copyt:
+                    continue  
+                else:
+                    copyt.pop()
+                
+            else: copyt.append(t[i])
+                
+        return "".join(copyt) == "".join(copys)
+                
+                
