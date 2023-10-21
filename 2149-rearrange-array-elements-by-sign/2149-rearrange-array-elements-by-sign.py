d6de66b1613a68ce36adab0a545970d6f4616de5
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-21 22:14:47 +0300

    Time: 1191 ms (58.25%), Space: 46.8 MB (76.78%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,29 +1,58 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        
-        po , ne , s = [] , [] , []
-        
-        
-        
-        for r in range(len(nums)):
-            
-            if nums[r] > 0: 
-                po.append(nums[r]) 
-            else: 
-                ne.append(nums[r])
-                    
-        pos_pointer = 0
-        neg_pointer = 0
-        
-        for r in range(len(nums)):
-            if r % 2 == 0: 
-                s.append(po[pos_pointer])
-                pos_pointer +=1
+        n = len(nums)
+        result = [0]*n
+        pos = 0
+        neg = 1
+        for num in nums:
+            if num > 0:
+                result[pos] = num
+                pos += 2
             else:
-                s.append(ne[neg_pointer])
-                neg_pointer +=1
+                result[neg] = num
+                neg += 2
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  previous implmentation not efficient
+# class Solution:
+#     def rearrangeArray(self, nums: List[int]) -> List[int]:
+        
+#         po , ne , s = [] , [] , []
+        
+        
+        
+#         for r in range(len(nums)):
+            
+#             if nums[r] > 0: 
+#                 po.append(nums[r]) 
+#             else: 
+#                 ne.append(nums[r])
+                    
+#         pos_pointer = 0
+#         neg_pointer = 1
+        
+#         for r in range(len(nums) , 2):
+            
+#             s.append(po[pos_pointer])
+#             pos_pointer +=1
+#                 s.append(ne[neg_pointer])
+#                 neg_pointer +=1
                 
-        return s
+#         return s
             
             
             
