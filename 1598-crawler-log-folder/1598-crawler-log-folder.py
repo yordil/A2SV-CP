2fb741afd223efce56187af0d1eca70e4b192199
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-21 14:52:35 +0300

    Time: 61 ms (16.06%), Space: 16.4 MB (58.21%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.py
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -1,19 +1,37 @@
 class Solution:
     def minOperations(self, logs: List[str]) -> int:
-        
         st = []
-        counter = 0
-        
-        for i in range(len(logs)):
-            
-            if logs[i] == "./":
-                continue
-            elif st and logs[i] == "../":
-                st.pop()
-            elif logs[i] != "../" and logs[i] != "./" :
-                st.append(logs[i])
-        
+
+        for lg in logs:
+            if lg != './':
+                if lg != '../':
+                    st.append(lg)
+                else:
+                    if st:
+                        st.pop()
         return len(st)
+
+
+
+#  my previous implmetation
+
+
+# class Solution:
+#     def minOperations(self, logs: List[str]) -> int:
+        
+#         st = []
+#         counter = 0
+        
+#         for i in range(len(logs)):
+            
+#             if logs[i] == "./":
+#                 continue
+#             elif st and logs[i] == "../":
+#                 st.pop()
+#             elif logs[i] != "../" and logs[i] != "./" :
+#                 st.append(logs[i])
+        
+#         return len(st)
                 
             
             
