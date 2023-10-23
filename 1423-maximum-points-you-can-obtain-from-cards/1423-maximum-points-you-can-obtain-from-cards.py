7c1ada8b1800dfd8a22928f94d47c782af0f03f2
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-23 16:37:48 +0300

    Time: 381 ms (20.64%), Space: 29.5 MB (66.38%) - LeetHub

---
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        
+        total = sum(cardPoints)
+        
+        Max = -1
+        cursum = 0
+        n = len(cardPoints)
+        
+        windowsum = 0
+        windowsize = n-k
+        
+        for i in range(n):
+            
+            windowsum += cardPoints[i]
+            
+            if i - windowsize >=0:
+                windowsum -= cardPoints[i-windowsize]
+                
+            if i >= windowsize - 1:
+                Max = max(Max , total-windowsum)
+                
+        return Max
+                
+            
+            
