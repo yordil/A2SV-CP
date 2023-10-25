b9e067e643d8e10790d2a0e1953fce89124182f9
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-25 11:57:30 +0300

    Time: 57 ms (92.47%), Space: 16.5 MB (9.52%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,15 +1,26 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
-         # create a string which is equal length with s1
-        
-        Str = s2[:len(s1)]
-        itr = 0 
-            
-        for i in range(len(s1) , len(s2)+1):
-            if sorted(Str) == sorted(s1):
+        if len(s1) > len(s2):
+            return False
+
+        s1_counts = [0] * 26
+        s2_counts = [0] * 26
+
+        # Initialize the character counts for s1 and the first window in s2
+        for i in range(len(s1)):
+            s1_counts[ord(s1[i]) - ord('a')] += 1
+            s2_counts[ord(s2[i]) - ord('a')] += 1
+
+        for i in range(len(s1), len(s2)):
+            if s1_counts == s2_counts:
                 return True
-            itr +=1
-            Str = s2[itr:i+1]
+
+            # Update the character count for the next window in s2
+            s2_counts[ord(s2[i]) - ord('a')] += 1
+            s2_counts[ord(s2[i - len(s1)]) - ord('a')] -= 1
+
+        # Check one last time after the loop
+        if s1_counts == s2_counts:
+            return True
+
         return False
-        
