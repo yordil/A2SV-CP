e4f80d3e91c49777f68c3e45a83e67becc0a308a
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-25 11:49:21 +0300

    Time: 40 ms (99.90%), Space: 29.9 MB (48.14%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,46 +1,35 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         
-        ans , visit  = set() , set()
-        Str = ""
+        myhash = {}
         
-        for l in range(len(s) -9):
-            Str = s[l:l+10]
-            if Str in visit:
-                ans.add(Str)
-            else:
-                visit.add(Str)
+        arr = []
+        Str = s[0:10]
+        L = 1
+        for i in range(10 ,len(s)+1):
+            if Str not in myhash:
+                myhash[Str] = 1
+            elif Str not in arr:
+                arr.append(Str)
                 
-        return list(ans)
+            Str = s[L:i+1]
+            L+=1
+        return arr
+                
+        
+#         ans , visit  = set() , set()
+#         Str = ""
+        
+#         for l in range(len(s) -9):
+#             Str = s[l:l+10]
+#             if Str in visit:
+#                 ans.add(Str)
+#             else:
+#                 visit.add(Str)
+                
+#         return list(ans)
     
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         myhash = {}
-        
-#         arr = []
-#         Str = s[0:10]
-#         L = 1
-#         for i in range(10 ,len(s)+1):
-#             if Str not in myhash:
-#                 myhash[Str] = 1
-#             elif Str not in arr:
-#                 arr.append(Str)
-                
-#             Str = s[L:i+1]
-#             L+=1
-#         return arr
-                
-        
-        
-        
-# # hash
+
