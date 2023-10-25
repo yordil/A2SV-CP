eb30ba359b91e8332b3838af9647d63d0a1f164e
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-25 11:48:17 +0300

    Time: 69 ms (42.92%), Space: 28.6 MB (62.45%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,21 +1,46 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         
-        myhash = {}
+        ans , visit  = set() , set()
+        Str = ""
         
-        arr = []
-        Str = s[0:10]
-        L = 1
-        for i in range(10 ,len(s)+1):
-            if Str not in myhash:
-                myhash[Str] = 1
-            elif Str not in arr:
-                arr.append(Str)
+        for l in range(len(s) -9):
+            Str = s[l:l+10]
+            if Str in visit:
+                ans.add(Str)
+            else:
+                visit.add(Str)
                 
-            Str = s[L:i+1]
-            L+=1
-        return arr
+        return list(ans)
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         myhash = {}
+        
+#         arr = []
+#         Str = s[0:10]
+#         L = 1
+#         for i in range(10 ,len(s)+1):
+#             if Str not in myhash:
+#                 myhash[Str] = 1
+#             elif Str not in arr:
+#                 arr.append(Str)
+                
+#             Str = s[L:i+1]
+#             L+=1
+#         return arr
                 
         
         
         
+# # hash
