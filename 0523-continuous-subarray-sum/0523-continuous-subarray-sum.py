711895ac63bc9beff769db94bcbccae30b027a50
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-10-27 17:44:21 +0300

    Time: 914 ms (92.69%), Space: 36 MB (54.31%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,18 +1,25 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         n = len(nums)
+        if n <2:
+            return False
+        
         
         prefix = 0 
         
-        my_dict = {0:0}
+        my_dict = {0:-1}
+        
         for i in range(n):
             prefix +=nums[i]
             
-            if prefix % k not in my_dict:
-                my_dict[prefix % k] = i+1
-            elif my_dict[prefix%k] < i:
-                return True
+            prefix %=k
+            if prefix in my_dict:
+                if i - my_dict[prefix]  > 1: 
+                    return True
+            else:
+                my_dict[prefix] = i
+            
             
         return False
                 
-            
+#             using map by initial value {0:0} and store [prefixsum % k] = i+1 else if dict[prefix % k] < i return True
