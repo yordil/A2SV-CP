b1492d805bcf622649a653c2885583aae318250d
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-01 21:59:16 +0300

    Time: 225 ms (42.07%), Space: 31.4 MB (66.32%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -1,16 +1,29 @@
-import math
+from collections import deque
 class ProductOfNumbers:
 
     def __init__(self):
         self.arr = []
+        self.prefix = []
 
     def add(self, num: int) -> None:
         self.arr.append(num)
+        if num == 0:
+            self.prefix = []
+        else:
+            if not self.prefix:
+                self.prefix.append(num)
+            else:
+                self.prefix.append(self.prefix[-1] * num)
 
     def getProduct(self, k: int) -> int:
-        return math.prod(self.arr[len(self.arr)-k : len(self.arr)])
-        
+        if k > len(self.prefix):
+            return 0
+        if k == len(self.prefix):
+            return self.prefix[-1]
+        return self.prefix[-1] // self.prefix[-k-1]
 
+        
+# we can keep product of last K element in the array and we can retrive it in O(1) time
 
 # Your ProductOfNumbers object will be instantiated and called as such:
 # obj = ProductOfNumbers()
