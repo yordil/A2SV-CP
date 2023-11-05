a3489e12ef7605963f7613be61dda4a39d101da3
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-06 02:04:59 +0300

    Time: 38 ms (91.76%), Space: 16.4 MB (10.39%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,24 +1,25 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        ans= [ ]
-        myhash = {}
+       
+    
+        hashidx = {}
+        ans = []
         size = 0
-        end = 0
-#         create an array to count the last index
-        for i, v in enumerate(s):
-            myhash[v] = i
-            
+        end= 0
+        
         for i , v in enumerate(s):
-            size += 1
-            end = max(end , myhash[v])
+            hashidx[v] = i
             
-                      
-                       
-            if i == end:
+        
+        for i in range(len(s)):
+            end = max(end , hashidx.get(s[i]))
+            size +=1
+            
+            if end == i:
                 ans.append(size)
-                size = 0
-            
-        return ans 
+                size=0
+                
+        return ans
             
         
         
