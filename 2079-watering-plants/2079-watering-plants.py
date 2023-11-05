da47fdf097b632fe7b9f25267322fc14ccff09db
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-06 00:39:43 +0300

    Time: 45 ms (95.47%), Space: 16.5 MB (36.99%) - LeetHub

---
+++ 2079-watering-plants/2079-watering-plants.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        
+        counter = 0
+        cap= capacity
+        left = 0
+        while left < len(plants):
+            
+            if cap >= plants[left]:
+                cap -= plants[left]
+                counter +=1
+                left +=1
+            else:
+                cap = capacity
+                counter += 2*left
+                
+        return counter
+            
+            
