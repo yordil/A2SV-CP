384c2b548f4db59cf7d21251c703c0cc43727bd8
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-07 17:41:03 +0300

    Time: 668 ms (36.41%), Space: 29.9 MB (52.17%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
@@ -3,17 +3,8 @@ class Solution:
         
         total = sum(chalk)
         remainder = k % total
-        
-#         prefix = [chalk[0]] * len(chalk)
-        
-#         for i in range(1, len(chalk)):
-#             prefix[i] = prefix[i-1] * chalk[i]
-            
-        
         for i in range(len(chalk)):
             if(chalk[i]) > remainder:
                 return i
             remainder -= chalk[i]
     
-#     3  7  8  10    ->  k=25 - 10 -8 -7 
-#  5  1  5 - > 5  6 11 ->   k = 22 
