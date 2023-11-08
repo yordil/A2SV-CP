8ed878c75549598cd1177194a899c8b68481e094
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-08 20:24:00 +0300

    Time: 454 ms (17.88%), Space: 18.9 MB (87.68%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,23 +1,43 @@
 from collections import deque
-
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        mywindow = deque()
-        zerocount = 0
-        maxlen = 0
-        
-        for i in range(len(nums)):
-            mywindow.append(nums[i])
-            
-            if nums[i] == 0:
-                zerocount += 1
-            
-            while zerocount > 1:
-                if mywindow.popleft() == 0:
-                    zerocount -= 1
-            
-            maxlen = max(maxlen, len(mywindow)-1)
+        zeroCount = 0
+        longestWindow = 0
+        start = 0
 
-        return maxlen
+        for i in range(len(nums)):
+            zeroCount += 1 if nums[i] == 0 else 0
+
+            while zeroCount > 1:
+                zeroCount -= 1 if nums[start] == 0 else 0
+                start += 1
+
+            longestWindow = max(longestWindow, i - start)
+
+        return longestWindow
+
+
+#  previous implmentation using O(N) - > space complexity
+
+
+# class Solution:
+#     def longestSubarray(self, nums: List[int]) -> int:
+#         mywindow = deque()
+#         zerocount = 0
+#         maxlen = 0
+        
+#         for i in range(len(nums)):
+#             mywindow.append(nums[i])
+            
+#             if nums[i] == 0:
+#                 zerocount += 1
+            
+#             while zerocount > 1:
+#                 if mywindow.popleft() == 0:
+#                     zerocount -= 1
+            
+#             maxlen = max(maxlen, len(mywindow)-1)
+
+#         return maxlen
 
             
