c717797f724a8f76fc53708b1b33ae531de6a87e
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-08 20:18:33 +0300

    Time: 448 ms (23.70%), Space: 19.1 MB (9.13%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,19 +1,23 @@
+from collections import deque
+
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        
-        if nums.count(1) == len(nums):
-            return len(nums) -1
         mywindow = deque()
         zerocount = 0
         maxlen = 0
+        
         for i in range(len(nums)):
             mywindow.append(nums[i])
-            while mywindow.count(0) > 1:
-                mywindow.popleft()
-                
-            maxlen = max(maxlen , len(mywindow)-1)
             
+            if nums[i] == 0:
+                zerocount += 1
             
-        
+            while zerocount > 1:
+                if mywindow.popleft() == 0:
+                    zerocount -= 1
+            
+            maxlen = max(maxlen, len(mywindow)-1)
+
         return maxlen
+
             
