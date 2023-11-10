e8a834f8ce7f7e4e4b0791f44cafc0b670ae51ff
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-10 18:04:14 +0300

    Time: 90 ms (90.48%), Space: 19.4 MB (86.70%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        arr =[]
+        arr = []
+        myhash = defaultdict(list)
+        for i in strs:   
+            sortedword = ''.join(sorted(i))
+            myhash[sortedword].append(i)
+        return list(myhash.values())
+            
