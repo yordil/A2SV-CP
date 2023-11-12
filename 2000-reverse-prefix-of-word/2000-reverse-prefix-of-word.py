f49d4c4985cb61eb969469e6f4c8cae11c729b05
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-12 21:48:11 +0300

    Time: 47 ms (5.59%), Space: 16.1 MB (72.74%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -2,9 +2,5 @@ class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         if ch not in word:
             return word
-        # ss = word.split("")
-        i = 0
-        for i in range(len(word)):
-            if word[i] == ch:
-                break
+        i = word.index(ch)
         return word[:i+1][::-1] + word[i+1:]
