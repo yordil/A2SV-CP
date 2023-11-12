6c49e3d75bcb653a154872676137197cc6f9376b
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-12 18:08:49 +0300

    Time: 153 ms (44.78%), Space: 17.2 MB (39.97%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -2,33 +2,72 @@ class Solution:
     def maxVowels(self, s: str, k: int) -> int:
         
         arr = [ 'a' , 'e' , 'i' , 'o'   , 'u']
-        counter = 0
+        best = 0
+        count = 0
         
-        ss = list(s)
-        
-        win = deque()
-        maxval = 0 
-        
-        for i in range(len(ss)):
-            win.append(ss[i])
-            if i < k:
-                if ss[i] in arr:
-                    counter +=1
+        for i in s[:k]:
+            if i in arr:
+                count+=1
+            if count > best:
+                best = count
+                    
+        for i in range(k , len(s)):
+            if s[i-k] in arr:
+                count -=1
+            if s[i] in arr:
+                count +=1
+            best = max(best, count)
             
-            if len(win) > k:
-                x = win.popleft()
-                if x in arr and ss[i] in arr:
-                    continue
-                elif x not in arr and ss[i] in arr:
-                    counter += 1
-                elif x in arr and ss[i] not in arr:
-                    counter -= 1
+        return best
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         arr = [ 'a' , 'e' , 'i' , 'o'   , 'u']
+#         counter = 0
+        
+#         ss = list(s)
+        
+#         win = deque()
+#         maxval = 0 
+        
+#         for i in range(len(ss)):
+#             win.append(ss[i])
+#             if i < k:
+#                 if ss[i] in arr:
+#                     counter +=1
+            
+#             if len(win) > k:
+#                 x = win.popleft()
+#                 if x in arr and ss[i] in arr:
+#                     continue
+#                 elif x not in arr and ss[i] in arr:
+#                     counter += 1
+#                 elif x in arr and ss[i] not in arr:
+#                     counter -= 1
                 
             
-            if len(win) == k:
-                maxval = max(counter , maxval)
+#             if len(win) == k:
+#                 maxval = max(counter , maxval)
                 
-        return maxval
+#         return maxval
                         
                 
             
