3eb3ffb51c39cc0fbf13bf5b7217c50a91a99f64
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-15 18:03:57 +0300

    Time: 58 ms (90.38%), Space: 16.4 MB (57.41%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -6,7 +6,7 @@ class Solution:
         s1_counts = [0] * 26
         s2_counts = [0] * 26
 
-        # Initialize the character counts for s1 and the first window in s2
+    
         for i in range(len(s1)):
             s1_counts[ord(s1[i]) - ord('a')] += 1
             s2_counts[ord(s2[i]) - ord('a')] += 1
@@ -15,11 +15,11 @@ class Solution:
             if s1_counts == s2_counts:
                 return True
 
-            # Update the character count for the next window in s2
+        
             s2_counts[ord(s2[i]) - ord('a')] += 1
             s2_counts[ord(s2[i - len(s1)]) - ord('a')] -= 1
 
-        # Check one last time after the loop
+      
         if s1_counts == s2_counts:
             return True
 
