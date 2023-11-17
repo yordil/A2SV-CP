af691f391a8fe77c3c83681ae08383e2b55b5c89
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-18 00:26:32 +0300

    Time: 185 ms (56.28%), Space: 28.1 MB (5.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,17 +1,22 @@
-from collections import deque 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        # place holder and seeker approach to solve this problem:
-        
-        d  = deque(nums)
-        
-        d.rotate(k)
-        
-        nums[:] = list(d)
-             
+        k = k % len(nums)
+        def helper(l , r):
+            while l < r:
+                nums[l] , nums[r] =  nums[r] , nums[l]
+                l+=1
+                r -=1
             
+        helper(0 , len(nums)-1)
+        helper(0 , k-1)
+        helper(k , len(nums)-1)
+        
+        
+        
+        
+        
+        
+            
+       
         
         
