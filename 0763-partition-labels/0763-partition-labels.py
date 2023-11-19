6a51acbae72b0e43ff9c34fb326d26536e3ad248
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-11-19 23:06:18 +0300

    Time: 42 ms (76.27%), Space: 16.3 MB (41.44%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,25 +1,21 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-       
-    
-        hashidx = {}
-        ans = []
-        size = 0
-        end= 0
-        
-        for i , v in enumerate(s):
-            hashidx[v] = i
-            
-        
-        for i in range(len(s)):
-            end = max(end , hashidx.get(s[i]))
-            size +=1
-            
-            if end == i:
-                ans.append(size)
-                size=0
-                
-        return ans
+        partitions = []
+        i = 0
+
+        while i < len(s):
+            char = s[i]
+            last_occurrence = s.rfind(char)
+
+            j = i + 1
+            while j < last_occurrence:
+                last_occurrence = max(last_occurrence, s.rfind(s[j]))
+                j += 1
+
+            partitions.append(last_occurrence - i + 1)
+            i = last_occurrence + 1
+
+        return partitions
             
         
         
