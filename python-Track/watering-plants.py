b18810c6b011111623f187fdb6176b0e85f4d363
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-12-04 18:17:41 +0300

    Added solution for Watering Plants

---
+++ python-Track/watering-plants.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        
+        ca = capacity
+        counter = 0
+        left = 0
+
+        while left < len(plants):
+
+            if ca >= plants[left]:
+                ca -= plants[left]
+                counter +=1
+                left +=1
+            else:
+                counter += 2*left
+                ca =capacity
+
+        return counter
+
+                
