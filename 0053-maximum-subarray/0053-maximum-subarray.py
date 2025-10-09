b538f7806bf828de0f90f984c03c41bc1a47ef2b
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2025-10-09 16:35:41 +0300

    Time: 95 ms (19.99%), Space: 32.9 MB (22.33%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,47 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        #using kaden's Algorithm can be implmented in O(n) time complexity
-        
-        cur = 0 
-        
-        maxval = nums[0]
-        
-        for n in nums:
-            cur = max(cur , 0)
-            
-            cur += n
-            
-            maxval = max(maxval , cur)
-            
-        return maxval
-    
-    
-#using sliding window also we can get the index too
+        maxSum , currentSum = nums[0] , nums[0]
+        ans  = 0
+        for i in range(1 , len(nums)):
 
-#         curr = 0 
-#         maxsum = nums[0]
-        
-#         maxl , maxr  ,  L = 0 , 0  , 0 
-        
-#         for r in range(len(nums)):
-            
-#             if cur < 0:
-#                 cur= 0
-#                 l = r
-                
-#             cur += num[r]
-            
-#             if cur > maxsum:
-#                 masxum=cur
-                
-#                 maxl , maxr = l , r
-                
-#             return [maxl , maxr]
-                
-                
-            
-        
+           
+            currentSum = max(nums[i], currentSum + nums[i])
+            maxSum = max(maxSum, currentSum)
 
-        
-      
+            
+
+        return maxSum
